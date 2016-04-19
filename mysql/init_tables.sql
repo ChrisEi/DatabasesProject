0bcf6804a7a503ee23e894f141336d40f8d0e430
notroot <cjewy6@mst.edu>
2016-04-19 12:36:18 -0500

    loadCards.js now fills the mtgcard database

--- mysql/init_tables.sql
+++ mysql/init_tables.sql
@@ -3,22 +3,22 @@ CREATE DATABASE magic;
 USE magic;
 
 CREATE TABLE mtgcard (
-  id VARCHAR(40),
+  id INTEGER(12) NOT NULL AUTO_INCREMENT,
   name VARCHAR(32),
   manaCost VARCHAR(12),
   cmc FLOAT(2,1),
-  superType VARCHAR(16),
   type VARCHAR(16),
-  subType VARCHAR(16),
   rarity VARCHAR(16),
-  cardText VARCHAR(32),
-  flavor VARCHAR(32),
-  num VARCHAR(8),
+  cardText VARCHAR(64),
+  flavor VARCHAR(128),
+  setNumber VARCHAR(4),
+  setName VARCHAR(32),
   power INTEGER(2),
   toughness INTEGER(2),
   hand VARCHAR(4),
   life VARCHAR(4),
   releaseDate VARCHAR(10),
+  setCode VARCHAR(5),
   PRIMARY KEY (id)
 );
 
--- mysql/loadCards.js
+++ mysql/loadCards.js
@@ -1,11 +1,134 @@
-var cards = require('./AllCards');
-var allCardNames = Object.keys(cards);
-var allCardKeys = []
+// Database connection code
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "pass",
+  database : 'magic'
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
 
 var sets = require('./AllSets');
 var allSetNames = Object.keys(sets);
-var allSetKeys = []
+var allCardKeys = []
 
+// Parse AllSets
+for (var i = 0; i < allSetNames.length; i++) {
+  // Parse the set data
+  var name = allSetNames[i];
+  var value = sets[name];
+  var keys = Object.keys(value);
+  var setCards = Object.keys(value.cards);
+  var set_Code = value.magicCardsInfoCode;
+  //console.log(value.magicCardsInfoCode);
+  //console.log(value.name)
+  //console.log(Object.keys(value.cards));
+  for (var j in setCards) {
+    //console.log(Object.keys(value.cards[j]));
+    // Parse each card in the set
+    var set_Name = value.name;
+    var set_Num = null;
+    if (value.cards[j].number) {
+      set_Num = value.cards[j].number;
+    }
+    var n = null;
+    if (value.cards[j].name) {
+      n = value.cards[j].name;
+    }
+    //console.log("Set: ", set_Name);
+    //console.log(set_Num, n);
+    var rare = null;
+    if (value.cards[j].rarity) {
+      rare = value.cards[j].rarity;
+    }
+    //console.log(rare);
+    var cmcost = null;
+    if (value.cards[j].cmc) {
+      cmcost = value.cards[j].cmc;
+    }
+    //console.log(cmcost);
+    var mC = null;
+    if (value.cards[j].manaCost) {
+      mC = value.cards[j].manaCost;
+    }
+  //  console.log(mC);
+    var ty = null;
+    if (value.cards[j].type) {
+      ty = value.cards[j].type;
+    }
+    //console.log(ty);
+    var text = null;
+    if (value.cards[j].text) {
+      text = value.cards[j].text;
+    }
+    //console.log(text);
+    var flav = null;
+    if (value.cards[j].flavor) {
+      flav = value.cards[j].flavor;
+    }
+    //console.log(flav);
+    var p = null;
+    if (value.cards[j].power) {
+      p = value.cards[j].power;
+    }
+    //console.log(p);
+    var t = null;
+    if (value.cards[j].toughness) {
+      t = value.cards[j].toughness;
+    }
+    //console.log(t);
+    var handCost = null;
+    if (value.cards[j].hand) {
+      handCost = value.cards[j].hand;
+    }
+    //console.log(handCost);
+    var lifeVal = null;
+    if (value.cards[j].life) {
+      lifeVal = value.cards[j].life;
+    }
+    //console.log(lifeVal);
+    var rDate = null;
+    if (value.cards[j].releaseDate) {
+      rDate = value.cards[j].releaseDate;
+    }
+    //console.log(rDate);
+
+
+    var card = { name: n, manaCost: mC, power: p, toughness: t, cmc: cmcost,
+                 type: ty, flavor: flav, rarity: rare, cardText: text,
+                 setNumber: set_Num, setName: set_Name, hand: handCost, life: lifeVal,
+                 releaseDate: rDate, setCode: set_Code};
+    con.query('INSERT INTO mtgcard SET ?', card, function(err,res){
+      if(err) throw err;
+
+      console.log('Last insert ID:', res.insertId);
+    });
+  }
+}
+//console.log(allCardKeys);
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
+
+
+
+// AllCards.json code:
+
+//var cards = require('./AllCards');
+//var allCardNames = Object.keys(cards);
+//var allCardKeys = []
+/*
 // Parse AllCards
 for (var i = 0; i < allCardNames.length; i++) {
   var name = allCardNames[i];
@@ -21,20 +144,5 @@ for (var i = 0; i < allCardNames.length; i++) {
   }
 }
 // output list of card keys
-console.log(allCardKeys);
-
-// Parse AllSets
-for (var i = 0; i < allSetNames.length; i++) {
-  var name = allSetNames[i];
-  var value = sets[name];
-  var keys = Object.keys(value);
-  console.log(value.magicCardsInfoCode);
-  console.log(value.cards)
-  for (var j in keys) {
-    if (allCardKeys.indexOf(keys[j]) < 0) {
-      allCardKeys.push(keys[j]);
-    }
-  }
-}
-// output list of set keys
-console.log(allCardKeys);
+//console.log(allCardKeys);
+*/
