a786b534bfe4e6b7608108aef265c1501aab90b6
notroot <cjewy6@mst.edu>
2016-04-19 15:21:09 -0500

    Changing html to jade (wip)

--- app.js
+++ app.js
@@ -3,59 +3,17 @@ var app = express();
 var path = require('path');
 var mysql = require('mysql');
 
-// This is a pool of database connections allowed
-var pool = mysql.createPool({
-  connectionLimit : 100,
-  host : 'localhost',
-  user : 'root',
-  password : 'pass',
-  database : 'magic'
-});
 
-// Simple get from db test
-function handle_database(req,res,type,query) {
-
-  pool.getConnection(function(err,connection){
-    if (err) {
-      connection.release();
-      res.json({"code" : 100, "status" : "error in connection  database"});
-      return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-
-    if(type == 'get') {
-      connection.query(query, function(err, rows) {
-        connection.release();
-        if(!err) {
-          res.json(rows);
-        }
-      });
-
-      connection.on('error', function(err) {
-        res.json({"code" : 100, "status" : "error in connection database"});
-        return;
-      });
-    }
-
-    if(type == 'post') {
-      connection.query(query, function(err, rows) {
-        connection.release();
-        if(!err) {
-          res.json(rows);
-        }
-      });
-    }
-  });
-}
-// end simple get from db test
 
 // Allow express to send static files from the public directory
 app.use(express.static(__dirname + '/public'));
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 // Establish get routes to the public files via url bar
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html')
+  //res.sendFile(__dirname + '/public/index.html')
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
 //  handle_database(req,res);
 });
 app.get('/profile', function(req, res) {
